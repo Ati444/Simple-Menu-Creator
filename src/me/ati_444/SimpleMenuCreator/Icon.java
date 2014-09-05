18b3804d641dc700a661c72ba107313e93c5d9d9
Ati444 <atifchaudry@sky.com>
2014-09-05 23:03:04 +0100

    Added Icon class

---
+++ src/me/ati_444/SimpleMenuCreator/Icon.java
@@ -0,0 +1,23 @@
+package me.ati_444.SimpleMenuCreator;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ * Represents an icon
+ * 
+ * @author Ati_444
+ * 
+ */
+public class Icon {
+
+	private ItemStack itemStack;
+	private ItemMeta itemMeta;
+
+	
+	public Icon(String displayName, Material material) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
